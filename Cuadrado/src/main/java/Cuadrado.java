bb3cc5df2134b54a1e0706016f9a0c4acb36376e
Aldo Torres Flores <aldo.torres1106@uppuebla.edu.mx>
2020-05-21 17:30:33 -0500

    Area y perimetro de un Cuadrado

---
+++ Cuadrado/src/main/java/Cuadrado.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+
+public class Cuadrado {
+    public static void main(String[] args) {
+        
+        Scanner leer=new Scanner(System.in);
+        double area, per;
+        
+        System.out.println("Ingresa el valor de un lado del cuadrado: ");
+        double lado=leer.nextDouble();
+        
+        area=lado*lado;
+        per=lado*4;
+        
+        System.out.println("El area del cuadrado es: " + area);
+        System.out.println("Su perimetro es de: " + per);
+        
+    }
+    
+}
