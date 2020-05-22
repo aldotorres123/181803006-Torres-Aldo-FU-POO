87ee5cf7dba790cfaa3655ccada73a47a592fb0b
Aldo Torres Flores <aldo.torres1106@uppuebla.edu.mx>
2020-05-22 08:16:12 -0500

    Hipotenusa

---
+++ Hipotenusa/src/main/java/Hipotenusa.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+
+public class Hipotenusa {
+    
+    public static void main(String[] args) {
+        
+        Scanner leer=new Scanner(System.in);
+        double a, b, c;
+        
+        System.out.println("Ingresa el cateto a: ");
+        a=leer.nextDouble();
+        System.out.println("Ingresa el cateto b: ");
+        b=leer.nextDouble();
+        
+        c=Math.sqrt(Math.pow(a, 2)+Math.pow(b, 2));
+        
+        System.out.println("La hipotenusa es: " + c);
+        
+    }
+    
+}
